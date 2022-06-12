47152ecddf3baf7c96fd711044e651fb912b7156
emirgundem <emirgndem@gmail.com>
2022-06-12 20:20:30 +0300

    typescript

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+let id = 5;
+let company = "test";
+let isPublished = true;
+let x = 'Hello';
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+let id: number = 5
+
+let company: string = "test"
+let isPublished: boolean = true
+
+let x: any = 'Hello'
+
+
+
+
+
+
