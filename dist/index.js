85b1dd6be91c7714f32edc8fb912a17a783c3dc3
emirgundem <emirgndem@gmail.com>
2022-06-12 20:25:13 +0300

    basic types

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,9 @@
 "use strict";
+//Basic Types
 let id = 5;
 let company = "test";
 let isPublished = true;
 let x = 'Hello';
+let age = 25;
+let ids = [1, 2, 3, 4, 5];
+let arr = [1, true, 'hello'];
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
+
+//Basic Types
 let id: number = 5
 
 let company: string = "test"
+
 let isPublished: boolean = true
 
 let x: any = 'Hello'
 
+let age:number = 25
 
+let ids: number[] = [1,2,3,4,5]
 
+let arr: any[] = [1,true,'hello']
 
 
 
