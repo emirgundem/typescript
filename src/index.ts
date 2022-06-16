ca7572bbd11db5cf8f77331311039220607169ca
emirgundem <emirgndem@gmail.com>
2022-06-16 11:33:04 +0300

    interfaces

--- src/index.ts
+++ src/index.ts
@@ -81,8 +81,26 @@ function log(message: string | number | boolean): void{
      console.log(message)
 }
 
+//Interfaces
 
+interface UserInterface {
+    readonly id: number
+    name: string
+    age?: number
+}
 
+const user1: UserInterface = {
+     id: 1,
+     name: 'John',
+
+}
+
+interface MathFunc {
+    (x: number, y:number): number
+}
+
+const add: MathFunc = (x:number,y:number): number => x + y
+const sub: MathFunc = (x:number,y:number): number => x-y
 
 
 
